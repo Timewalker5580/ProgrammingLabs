aa111df8b0b28750d0c97aa3831bdc33efe6c61d
Timewalker5580 <shaposhnikov5580@gmail.com>
2022-02-23 18:56:55 +0200

    Update Bike.java

--- Bike.java
+++ Bike.java
@@ -1,15 +1,3 @@
-// public class Main {
-//   public static void main(String[] args) {
-//     Bike firstBike = new Bike();
-//     Bike secondBike = new Bike(21, 23.4, 130, "Pinarello", 23000, "Andrew");
-//     Bike thirdBike = new Bike(23, 30.4, 110, "Electra");
-
-//     System.out.println(firstBike.toString());
-//     System.out.println(secondBike.toString());
-//     System.out.println(thirdBike.toString());
-//   }
-// }
-
 public record Bike(int qtyGear, double weight, int maxWeight, String brand, int price ,String owner) {
 
   private static final boolean isTwoWheels = true;
